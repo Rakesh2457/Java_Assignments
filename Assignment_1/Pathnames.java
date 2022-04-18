451f0df06490512ddfc16cc9cc12709964fd5cdc
Rakesh2457 <102479671+Rakesh2457@users.noreply.github.com>
2022-04-18 08:05:19 +0530

    Add files via upload

--- Assignment_1/Pathnames.java
+++ Assignment_1/Pathnames.java
@@ -1,1 +1,31 @@
+import java.io.File;
+import java.util.Scanner;
+import java.util.regex.*;
 
+public class Pathnames {
+    public static void main(String[] args) {
+        File file = new File("C:\\OneDrive\\Desktop\\Demo");
+        Scanner KeyBoard = new Scanner(System.in);
+        System.out.println("Enter the file to be search ");
+        String fileToSearch = KeyBoard.nextLine();
+        int flag = 0;
+        Pattern pattern = Pattern.compile(fileToSearch);
+
+
+        for (File listOfFiles : file.listFiles()) {
+            //System.out.println("----"+listOfFiles);
+            Matcher matcher = pattern.matcher(String.valueOf(listOfFiles));
+            boolean matchFound = matcher.find();
+            if (matchFound==true) {
+                File f=new File(file,fileToSearch);
+
+                System.out.println("file found path is : " +  f.getAbsolutePath());
+                flag = 1;
+                break;
+            }
+        }
+        if(flag==0)
+            System.out.println("file not found");
+    }
+
+}
