6dd7cd673ec906e95b9ba6bf3af968feb64c3ba7
Rakesh2457 <102479671+Rakesh2457@users.noreply.github.com>
2022-04-18 08:07:33 +0530

    Add files via upload

--- Assignment_2/IsAlpha.java
+++ Assignment_2/IsAlpha.java
@@ -1,1 +1,31 @@
+package IS_ALPHA;
 
+public class IsAlpha {
+
+    public static boolean isAlphabetString(String s)
+    {
+        if (s == null) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if (!(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        String s = "Rakesh12";
+        if (isAlphabetString(s)){
+            System.out.println("The Given String contains only alphabets");
+        }
+        else {
+            System.out.println("The given String has other than alphabets also");
+        }
+    }
+}
